2eb62eaca759f4438243f642a86c7f5e07231d97
cutelee <soyoung.dev@gmail.com>
2019-07-01 16:01:50 +0900

    register, login API 구현
    - JWT 생성 후 로그인 시 반환

--- routes/api/auth/controller.js
+++ routes/api/auth/controller.js
@@ -1,3 +1,8 @@
+const jwt = require('jsonwebtoken');
+
+// models load
+const User = require('../../../models/user');
+
 /*
   POST /api/auth/register
   {
@@ -5,7 +10,114 @@
     password
   }
 */
-
 exports.register = (req, res) => {
-  res.send('This router is working')
+  const { username, password } = req.body;
+
+  console.log("body:", req.body);
+  console.log("username:", username);
+  let newUser = null;
+
+  const create = user => {
+    console.log("user:", user);
+    if (user) {
+      throw new Error("User already exists.");
+    } else {
+      return User.create(username, password);
+    }
+  };
+
+  const count = user => {
+    newUser = user;
+    return User.count({}).exec();
+  };
+
+  const assign = count => {
+    if (count === 1) {
+      return newUser.assignAdmin();
+    } else {
+      return Promise.resolve(false);
+    }
+  };
+
+  const respond = isAdmin => {
+    res.json({
+      message: "Registered successfully",
+      admin: !!isAdmin,   // isAdmin ? true : false
+    });
+  };
+
+  const onError = error => {
+    res.status(409).json({
+      message: error.message,
+    });
+  };
+
+  User.findOneByUsername(username)
+    .then(create)
+    .then(count)
+    .then(assign)
+    .then(respond)
+    .catch(onError);
+};
+
+/*
+  POST /api/auth/login
+  {
+    username,
+    password
+  }
+ */
+exports.login = (req, res) => {
+  const { username, password } = req.body;
+  const secret = req.app.get('jwt-secret');
+
+  const check = user => {
+    if (!user) {
+      throw new Error("User doesn't exist.");
+    } else {
+      if (user.verify(password)) {
+        return new Promise((resolve, reject) => {
+          jwt.sign(
+            {
+              _id: user._id,
+              username: user.username,
+              admin: user.admin,
+            },
+            secret,
+            {
+              expiresIn: "7d",
+              issuer: "cutelee.com",
+              subject: "userInfo",
+            },
+            (err, token) => {
+              if (err) {
+                reject(err);
+              }
+              resolve(token);
+            }
+          );
+        });
+      } else {
+        throw new Error("Login failed: Wrong password");
+      }
+    }
+  };
+
+  const respond = token => {
+    res.json({
+      message: "Logged in successfully.",
+      token,
+    });
+  };
+
+  const onError = error => {
+    res.status(403).json({
+      message: error._message,
+    });
+  };
+
+  User.findOneByUsername(username)
+    .then(check)
+    .then(respond)
+    .catch(onError);
 };
