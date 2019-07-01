956aeeeb77daa76f1ffadb1835957c2e36f27bbe
cutelee <soyoung.dev@gmail.com>
2019-07-01 16:53:49 +0900

    user 목록 조회용 API 생성, jwt 검증
    - api/index.js에서 /user 모든 요청 jwt 검증

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
+
+const authMiddleware = require('../../middlewares/auth');
 const auth = require('./auth');
+const user = require('./user');
 
 router.use('/auth', auth);
+// user 요청이 들어오면 authMiddleware 거친 후, user 라우터로 감
+// 라우터 내부 모든 API 가 검증이 필요하므로 user/index.js가 아니라 여기에서 함
+router.use('/user', authMiddleware);
+router.use('/user', user);
 
 module.exports = router;
---
+++ src/routes/api/user/controller.js
@@ -0,0 +1,32 @@
+const User = require('../../../models/user');
+
+/*
+  GET /api/user/list
+ */
+exports.list = (req, res) => {
+  if (!req.decoded.admin) {
+    return res.status(403).json({
+      message: "You are not an admin.",
+    });
+  }
+
+  User.find({})
+    .then(users => {
+      res.json({ users });
+    })
+};
+
+/*
+  POST /api/user/assign-admin/:username
+ */
+exports.assignAdmin = (req, res) => {
+  if (!req.decoded.admin) {
+    return res.status(403).json({
+      message: "You are not an admin.",
+    });
+  }
+
+  User.findOneByUsername(req.params.username)
+    .then(user => user.assignAdmin)
+    .then(res.json({ success: true }));
+};
